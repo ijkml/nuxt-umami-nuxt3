8b226c66ff12d1aac99afb00daa97b43538204e7
ML <me.mlaure@gmail.com>
2022-12-11 18:20:49 +0100

    configure `umami`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// import NuxtUmami from 'nuxt-umami';
+
 export default defineNuxtConfig({
   modules: [
     '@unocss/nuxt',
+    'nuxt-umami',
   ],
+  umami: {
+    // enable: false,
+    autoTrack: false,
+    scriptUrl: 'http://localhost:6000/umami.js',
+    websiteId: '5e1eeffc-821f-4d52-a214-e357c1e39ad5',
+  },
+  app: {
+    head: {
+      title: 'Nuxt Umami Testing',
+    },
+  },
   css: ['@unocss/reset/tailwind.css'],
 });
