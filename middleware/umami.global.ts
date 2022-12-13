7f27c8b2b11b06a99c8b6b3b6f5f2f2c0c0f7144
ML <me.mlaure@gmail.com>
2022-12-13 19:23:12 +0100

    implement exclusion

--- middleware/umami.global.ts
+++ middleware/umami.global.ts
@@ -1,4 +1,18 @@
+import matcher from '@ijkml/minimatch';
+
+const excludeList = [
+  '/private',
+  '/dashboard',
+  '/dashboard/*',
+];
+
 export default defineNuxtRouteMiddleware((to) => {
   const { $umami } = useNuxtApp();
-  $umami.trackView(to.fullPath);
+
+  const excluded = excludeList.map(item => matcher.match(item, to.href));
+  if (excluded.includes(true)) {
+    return;
+  }
+
+  $umami.trackView(to.href);
 });
